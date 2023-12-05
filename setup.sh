8fb31199796d6f21ce7f310e1bba40c825f75da2
Philip Reinhart <74872363+philiprein@users.noreply.github.com>
2023-12-05 20:38:14 +0100

    remove more macos specific stuff

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env zsh
+#!/bin/zsh
 
 # ###########################################################
 # Preparations
@@ -13,8 +13,6 @@ set -eo pipefail
 
 DOTFILES_DIR="$HOME/.dotfiles"
 BIN_DIR="$HOME/.bin"
-# Disable Gatekeeper for casks installed by Homebrew
-export HOMEBREW_CASK_OPTS="--no-quarantine"
 
 # ###########################################################
 # Define Functions
@@ -23,11 +21,6 @@ export HOMEBREW_CASK_OPTS="--no-quarantine"
 # Function to determine if the provided command is executable
 function is-executable() {
   return $(whence $1 &>/dev/null)
-}
-
-# Function to determine if the underlying OS is macOS based on the presence of a command specific to macOS, 'sw_vers'
-function is-macos() {
-  return $(whence sw_vers &>/dev/null)
 }
 
 # Robust symlinking
@@ -55,33 +48,6 @@ function symlink() {
   fi
   ln -sf "$src" "$dest"
 }
-
-# ###########################################################
-# Install Xcode Command Line Tools
-# ###########################################################
-
-echo "Installing Xcode Command Line Tools..."
-# Check if Xcode Command Line Tools are already installed
-if ! xcode-select --print-path &>/dev/null; then
-
-  # Prompt user to install the Xcode Command Line Tools
-  xcode-select --install &>/dev/null
-
-  # Wait until the Xcode Command Line Tools are installed
-  until xcode-select --print-path &>/dev/null; do
-    sleep 5
-  done
-  if [[ $? != 0 ]]; then
-    error "Unable to install Xcode Command Line Tools, exiting..."
-    exit 2
-  else
-    echo 'Xcode Command Line Tools installed successfully.'
-    # Prompt user to agree to the terms of the Xcode license
-    sudo xcodebuild -license
-  fi
-else
-  echo "Skipping install of Xcode Command Line Tools. They are already installed."
-fi
 
 # ###########################################################
 # Symlinking Dotfiles
